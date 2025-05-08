ab5d011d771b9388f719da67fd9604399808b74f
skyter10086 <skyter10086@aliyun.com>
2025-05-08 16:01:45 +0800

    Add files via upload
    updated summary.py

---
+++ export-data.py
@@ -0,0 +1,241 @@
+# tmp.py
+
+import xlwings as xw
+import pandas as pd
+from dbfread2 import DBF
+
+
+# Paths 文件路径"
+path_a = r"E:\企业补贴\数据\企业补贴202505"
+path_b = r"E:\企业补贴\数据\集体工企业补贴202505"
+dbf_path_a = path_a + r"\bt_ltx.dbf"
+dbf_path_b = path_b + r"\bt_ltx.dbf"
+temp_path_icbc = r"E:\企业补贴\银行报盘\工商银行报盘模板.xlsx"
+temp_path_cbc = r"E:\企业补贴\银行报盘\建设银行报盘模板.xlsx"
+temp_path_boc = r"E:\企业补贴\银行报盘\中国银行报盘模板.xlsx"
+temp_path_bocny = r"E:\企业补贴\银行报盘\中国银行南阳报盘模板.xlsx"
+
+
+# 老人补贴
+def export_datagrid(
+    dbf_path: str,
+    template: str,
+    sheet: str,
+    arch: str,
+    add_columns: dict,
+    condition: str,
+    columns: list[str],
+    output: str,
+    index=False,
+):
+    dbf = DBF(dbf_path)
+    df = pd.DataFrame(iter(dbf))
+
+    app = xw.App(visible=False, add_book=False)
+    wb = app.books.open(template)
+    sht = wb.sheets[sheet]
+
+    for k, v in add_columns.items():
+        df[k] = v
+
+    result = df.query(condition).filter(items=columns)
+    if index == True:
+        result = result.reset_index(drop=True)
+        result.index = result.index + 1
+        result = result.reset_index()
+
+    data = result.values.tolist()
+    sht.range(arch).value = data
+
+    wb.save(output)
+    wb.close()
+    app.quit()
+
+
+# 老人企业补贴 -- 工行跨行
+export_datagrid(
+    dbf_path=dbf_path_a,
+    template=temp_path_icbc,
+    sheet="工行跨行",
+    arch="A2",
+    add_columns={"行别": 1, "业务种类": "00602", "协议书号": ""},
+    condition="RE == 1 and 发放银行 == '工行异地'",
+    columns=[
+        "姓名",
+        "X_银行帐号",
+        "行别",
+        "银行帐号",
+        "业务种类",
+        "协议书号",
+        "发放地点",
+        "实发补贴",
+    ],
+    output=r"E:\企业补贴\银行报盘\2025年\5月\工行报盘\老人企业补贴(工行报盘-跨行)_202505.xlsx",
+)
+
+# 老人企业补贴 -- 工行本行
+export_datagrid(
+    dbf_path=dbf_path_a,
+    template=temp_path_icbc,
+    sheet="工行跨行",
+    arch="A2",
+    add_columns={
+        "行别": "",
+        "跨行行号": "",
+        "业务种类": "",
+        "协议书号": "",
+        "账号地址": "",
+    },
+    condition="RE == 1 and 发放银行 == '工商银行'",
+    columns=[
+        "姓名",
+        "X_银行帐号",
+        "行别",
+        "跨行行号",
+        "业务种类",
+        "协议书号",
+        "账号地址",
+        "实发补贴",
+    ],
+    output=r"E:\企业补贴\银行报盘\2025年\5月\工行报盘\老人企业补贴(工行报盘-本行)_202505.xlsx",
+)
+
+# 老人企业补贴 -- 建设银行
+export_datagrid(
+    dbf_path=dbf_path_a,
+    template=temp_path_cbc,
+    sheet="sheet1",
+    arch="A2",
+    add_columns={
+        "行别": "",
+        "跨行行号": "",
+        "业务种类": "",
+        "协议书号": "",
+        "账号地址": "",
+    },
+    condition="RE == 1 and 发放银行 == '建设银行'",
+    columns=["X_银行帐号", "姓名", "实发补贴"],
+    index=True,
+    output=r"E:\企业补贴\银行报盘\2025年\5月\建行报盘\老人企业补贴(建行报盘)_202505.xlsx",
+)
+
+# 老人企业补贴 -- 中行南阳
+export_datagrid(
+    dbf_path=dbf_path_a,
+    template=temp_path_bocny,
+    sheet="sheet1",
+    arch="A2",
+    add_columns={
+        "行别": "",
+        "跨行行号": "",
+        "业务种类": "",
+        "协议书号": "",
+        "账号地址": "",
+    },
+    condition="RE == 1 and 发放银行 == '中国银行_南阳'",
+    columns=["姓名", "身份证", "发放银行", "X_银行帐号", "实发补贴"],
+    index=True,
+    output=r"E:\企业补贴\银行报盘\2025年\5月\中行报盘\老人企业补贴(中行南阳报盘)_202505.xlsx",
+)
+
+# 集体工企业补贴 -- 工行跨行
+export_datagrid(
+    dbf_path=dbf_path_b,
+    template=temp_path_icbc,
+    sheet="工行跨行",
+    arch="A2",
+    add_columns={"行别": 1, "业务种类": "00602", "协议书号": ""},
+    condition="RE == 1 and 发放银行 in ('工商银行异地','商业银行（工行代发）')",
+    columns=[
+        "姓名",
+        "X_银行帐号",
+        "行别",
+        "银行帐号",
+        "业务种类",
+        "协议书号",
+        "发放地点",
+        "实发补贴",
+    ],
+    output=r"E:\企业补贴\银行报盘\2025年\5月\工行报盘\集体工企业补贴(工行报盘-跨行)_202505.xlsx",
+)
+
+# 集体工企业补贴 -- 工行本行
+export_datagrid(
+    dbf_path=dbf_path_b,
+    template=temp_path_icbc,
+    sheet="工行跨行",
+    arch="A2",
+    add_columns={
+        "行别": "",
+        "跨行行号": "",
+        "业务种类": "",
+        "协议书号": "",
+        "账号地址": "",
+    },
+    condition="RE == 1 and 发放银行 == '工商银行'",
+    columns=[
+        "姓名",
+        "X_银行帐号",
+        "行别",
+        "跨行行号",
+        "业务种类",
+        "协议书号",
+        "账号地址",
+        "实发补贴",
+    ],
+    output=r"E:\企业补贴\银行报盘\2025年\5月\工行报盘\集体工企业补贴(工行报盘-本行)_202505.xlsx",
+)
+
+# 集体工企业补贴 -- 建设银行
+export_datagrid(
+    dbf_path=dbf_path_b,
+    template=temp_path_cbc,
+    sheet="sheet1",
+    arch="A2",
+    add_columns={
+        "行别": "",
+        "跨行行号": "",
+        "业务种类": "",
+        "协议书号": "",
+        "账号地址": "",
+    },
+    condition="RE == 1 and 发放银行 == '建设银行'",
+    columns=["X_银行帐号", "姓名", "实发补贴"],
+    index=True,
+    output=r"E:\企业补贴\银行报盘\2025年\5月\建行报盘\集体工企业补贴(建行报盘)_202505.xlsx",
+)
+
+# 集体工企业补贴 -- 中行南阳
+export_datagrid(
+    dbf_path=dbf_path_b,
+    template=temp_path_bocny,
+    sheet="sheet1",
+    arch="A2",
+    add_columns={
+        "行别": "",
+        "跨行行号": "",
+        "业务种类": "",
+        "协议书号": "",
+        "账号地址": "",
+    },
+    condition="RE == 1 and 发放银行 == '中国银行_南阳'",
+    columns=["姓名", "身份证", "发放银行", "X_银行帐号", "实发补贴"],
+    index=True,
+    output=r"E:\企业补贴\银行报盘\2025年\5月\中行报盘\集体工企业补贴(中行南阳报盘)_202505.xlsx",
+)
+
+# 集体工企业补贴 -- 中行油区
+export_datagrid(
+    dbf_path=dbf_path_b,
+    template=temp_path_boc,
+    sheet="sheet1",
+    arch="A2",
+    add_columns={
+        "开户行": "中国银行",
+        "行号": "41",
+    },
+    condition="RE == 1 and 发放银行 == '中国银行_油区'",
+    columns=["实发补贴", "姓名", "X_银行帐号", "开户行", "行号"],
+    index=False,
+    output=r"E:\企业补贴\银行报盘\2025年\5月\中行报盘\集体工企业补贴(中行油区报盘)_202505.xlsx",
+)
--- summary.py
+++ summary.py
@@ -34,24 +34,35 @@ def build_data(dwbm, dbf, on_field, with_how) -> list[list]:
 
     return result
 
+def export_data(template:str,sheets:list[dict],output:str):
+    wb = load_workbook(filename = template)
+
+    for sheet in sheets:
+        
+        sht_name = sheet['name']
+        sht_title = sheet['title']
+        sht_arch = sheet['arch']
+        data = sheet['data']
+
+        ws = wb[sht_name]
+        ws[sht_title['cell']]=sht_title['value']
+        fill(ws,sht_arch,data)
+
+    wb.save(output)
+
 
 def main():
+    # 老人企业补贴
     dbf = read_dbf(
-        filename=r"E:\企业补贴\数据\企业补贴202505\bt_ltx.dbf",
+        filename=r"E:\企业补贴\数据\企业补贴202505\bt_ltx.dbf", # dbf
         column_names=["DWBM", "实发补贴"],
         group_by="DWBM",
         agg_dict={"实发补贴": ["count", "sum"]},
         alias=["单位编码", "实发人数", "企业补贴发放额"],
     )
     dwbm = read_dwbm(r"E:\企业补贴\银行报盘\全民dwbm.xlsx")
-    data = build_data(dwbm, dbf, "单位编码", "left").values.tolist()
-    wb = load_workbook(
-        filename=r"E:\企业补贴\银行报盘\离退休职工企业补贴汇总模板（老人）.xlsx"
-    )
-    ws = wb["单位汇总"]
-    ws["A1"] = "2025年5月全民离退休人员企业补贴发放汇总表(单位)"
-    ws_1 = wb["银行汇总"]
-    ws_1["A1"] = "2025年5月全民离退休人员企业补贴发放汇总表(银行)"
+    data_qm = build_data(dwbm, dbf, "单位编码", "left").values.tolist()
+
     dbf_1 = read_dbf(
         filename=r"E:\企业补贴\数据\企业补贴202505\bt_ltx.dbf",
         column_names=["发放银行", "X_银行帐号", "补发_其它", "其它扣款", "实发补贴"],
@@ -65,12 +76,36 @@ def main():
         alias=["发放银行", "X_银行帐号", "补发_其它", "其它扣款", "企业补贴发放额"],
     )
     bank_data = dbf_1.values.tolist()
-    fill(ws_1, "G4", bank_data)
-    fill(ws, "B4", data)
-    wb.save(r"E:\企业补贴\银行报盘\2025年\5月\老人企业补贴汇总202505.xlsx")
+    export_data(
+        template=r"E:\企业补贴\银行报盘\离退休职工企业补贴汇总模板（老人）.xlsx",
+        sheets=[
+            {
+                "name":"单位汇总",
+                "title": {
+                    "cell":"A1",
+                    "value":"2025年5月全民离退休人员企业补贴发放汇总表(单位)"
+                },
+                "arch":"B4",
+                "data": data_qm,
+            },
+            {
+                "name":"银行汇总",
+                "title": {
+                    "cell":"A1",
+                    "value":"2025年5月全民离退休人员企业补贴发放汇总表(银行)"
+                },
+                "arch":"G4",
+                "data": bank_data,
+            }
+        ],
+        output=r"E:\企业补贴\银行报盘\2025年\5月\老人企业补贴汇总202505.xlsx",
+    )
 
+
+
+    # 集体工企业补贴
     dbf2 = read_dbf(
-        filename=r"E:\企业补贴\数据\集体工企业补贴202505\bt_ltx.dbf",
+        filename=r"E:\企业补贴\数据\集体工企业补贴202505\bt_ltx.dbf", # dbf
         column_names=["DWBM", "X_银行帐号", "补发_其它", "实发补贴"],
         group_by="DWBM",
         agg_dict={
@@ -80,33 +115,22 @@ def main():
         },
         alias=["单位编码", "实发人数", "提高待遇", "实发金额"],
     )
-    wb2 = load_workbook(
-        filename=r"E:\企业补贴\银行报盘\退休集体工企业补贴汇总模板.xlsx"
-    )
-    ws2 = wb2["企业补贴（财务拨款）"]
-    ws2["A1"] = "2025年5月退休集体工企业补贴发放汇总表"
-    ws_2 = wb2["企业补贴（含遗孀）"]
-    ws_2["A1"] = "2025年5月退休集体工企业补贴发放汇总表(含遗孀)"
     dwbm2 = read_dwbm(filename=r"E:\企业补贴\银行报盘\非全民dwbm.xlsx")
     data2 = build_data(dwbm=dwbm2, dbf=dbf2, on_field="单位编码", with_how="left")
     data2["企业补贴"] = data2["实发金额"] - data2["提高待遇"]
+    
     data2f = data2.filter(
         items=["单位名称", "实发人数", "企业补贴", "提高待遇", "实发金额", "性质"]
     )
     fqm = data2f[data2f["性质"] != "工程"]
+    fqm_data = fqm.values.tolist()
+    
     data2f_ = data2.filter(
         items=["单位名称", "实发人数", "提高待遇", "实发金额", "性质"]
     )
     gc = data2f_[data2f_["性质"] == "工程"]
-    fqm_data = fqm.values.tolist()
     gc_data = gc.values.tolist()
-    ws3 = wb2["企业补贴（单位征集）"]
-    ws3["A1"] = "2025年5月退休集体工企业补贴发放汇总表(工程)"
-    fill(ws2, "B4", fqm_data)
-    fill(ws_2, "B4", fqm_data)
-    fill(ws3, "J4", gc_data)
-    ws_2 = wb2["企业补贴（银行汇总）"]
-    ws_2["A1"] = "2025年5月退休集体工企业补贴发放汇总表(银行)"
+
     dbf_2 = read_dbf(
         filename=r"E:\企业补贴\数据\集体工企业补贴202505\bt_ltx.dbf",
         column_names=["发放银行", "X_银行帐号", "补发_其它", "实发补贴"],
@@ -119,9 +143,87 @@ def main():
         alias=["发放银行", "实发人数", "提高待遇", "实发金额"],
     )
     bank_data2 = dbf_2.values.tolist()
-    fill(ws_2, "H4", bank_data2)
-    wb2.save(r"E:\企业补贴\银行报盘\2025年\5月\退休集体工企业补贴汇总202505.xlsx")
 
+    export_data(
+        template=r"E:\企业补贴\银行报盘\退休集体工企业补贴汇总模板.xlsx",
+        sheets=[
+            {
+                "name":"企业补贴（财务拨款）",
+                "title": {
+                    "cell":"A1",
+                    "value":"2025年5月退休集体工企业补贴发放汇总表"
+                },
+                "arch":"B4",
+                "data": fqm_data,
+            },
+            {
+                "name":"企业补贴（含遗孀）",
+                "title": {
+                    "cell":"A1",
+                    "value":"2025年5月退休集体工企业补贴发放汇总表(含遗孀)"
+                },
+                "arch":"B4",
+                "data": fqm_data,
+            },
+            {
+                "name":"企业补贴（单位征集）",
+                "title": {
+                    "cell":"A1",
+                    "value":"2025年5月退休集体工企业补贴发放汇总表(工程)"
+                },
+                "arch":"J4",
+                "data": gc_data,
+            },
+            {
+                "name":"企业补贴（银行汇总）",
+                "title": {
+                    "cell":"A1",
+                    "value":"2025年5月退休集体工企业补贴发放汇总表(银行)"
+                },
+                "arch":"H4",
+                "data": bank_data2,
+            }
+        ],
+        output=r"E:\企业补贴\银行报盘\2025年\5月\退休集体工企业补贴汇总202505.xlsx",
+    )
+    
+
+
+
+    
 
 if __name__ == "__main__":
     main()
+
+
+
+"""
+    wb = load_workbook(
+        filename=r"E:\企业补贴\银行报盘\离退休职工企业补贴汇总模板（老人）.xlsx"
+    )
+    ws = wb["单位汇总"]
+    ws["A1"] = "2025年5月全民离退休人员企业补贴发放汇总表(单位)" # title
+    ws_1 = wb["银行汇总"]
+    ws_1["A1"] = "2025年5月全民离退休人员企业补贴发放汇总表(银行)" # title
+    
+    fill(ws_1, "G4", bank_data)
+    fill(ws, "B4", data)
+    wb.save(r"E:\企业补贴\银行报盘\2025年\5月\老人企业补贴汇总202505.xlsx") # file
+    wb2 = load_workbook(
+        filename=r"E:\企业补贴\银行报盘\退休集体工企业补贴汇总模板.xlsx"
+    )
+    ws2 = wb2["企业补贴（财务拨款）"]
+    ws2["A1"] = "2025年5月退休集体工企业补贴发放汇总表" # title
+    ws_2 = wb2["企业补贴（含遗孀）"]
+    ws_2["A1"] = "2025年5月退休集体工企业补贴发放汇总表(含遗孀)" # title
+    ws3 = wb2["企业补贴（单位征集）"]
+    ws3["A1"] = "2025年5月退休集体工企业补贴发放汇总表(工程)" # title
+    fill(ws2, "B4", fqm_data)
+    fill(ws_2, "B4", fqm_data)
+    fill(ws3, "J4", gc_data)
+    ws_2 = wb2["企业补贴（银行汇总）"]
+    ws_2["A1"] = "2025年5月退休集体工企业补贴发放汇总表(银行)" # title
+    fill(ws_2, "H4", bank_data2)
+    wb2.save(r"E:\企业补贴\银行报盘\2025年\5月\退休集体工企业补贴汇总202505.xlsx") # file
+
+"""
