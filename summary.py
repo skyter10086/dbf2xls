c18f0deda13debbe85b1ce38909fbb842c843ee6
skyter10086 <skyter10086@aliyun.com>
2025-05-08 16:51:11 +0800

    Update summary.py
    remove reportman

--- summary.py
+++ summary.py
@@ -1,7 +1,15 @@
-from reportman import *
 from dbfread2 import DBF
 import pandas as pd
+from openpyxl.worksheet.worksheet import Worksheet
 
+
+def fill(ws: Worksheet, start_cell: str, data_list: list[list]):
+    start_row = int(start_cell[1:])
+    start_col = ord(start_cell[0]) - ord("A") + 1
+
+    for i, row_data in enumerate(data_list):
+        for j, cell_value in enumerate(row_data):
+            ws.cell(row=start_row + i, column=start_col + j, value=cell_value)
 
 def read_dbf(
     filename: str,
